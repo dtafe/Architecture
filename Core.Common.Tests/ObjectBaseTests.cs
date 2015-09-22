c109ca86f82cf61a54797a24f22242a25fa52006
thienchuong9x <thienchuong.la91@gmail.com>
2015-09-22 09:30:04 +0700

    test

--- Core.Common.Tests/ObjectBaseTests.cs
+++ Core.Common.Tests/ObjectBaseTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,58 @@ namespace Core.Common.Tests
     [TestClass]
     public class ObjectBaseTests
     {
-        TestClass objTest = new TestClass();
+        public void test_clean_property_change()
+        {
+            TestClass objTest = new TestClass();
+            bool propertyChange = false;
+            objTest.PropertyChanged += (s, e) =>
+                 { if (e.PropertyName == "CleanProp") propertyChange = true; };
+            objTest.CleanProp = "test value";
+            Assert.IsTrue(propertyChange, "The property should have triggered change notification.");
+        }
+        [TestMethod]
+        public void test_dirty_set()
+        {
+            TestClass objTest = new TestClass();
+
+            Assert.IsFalse(objTest.IsDirty, "Object should be clean.");
+
+            objTest.DirtyProp = "test value";
+
+            Assert.IsTrue(objTest.IsDirty, "Object should be dirty.");
+        }
+
+        [TestMethod]
+        public void test_property_change_single_subscription()
+        {
+            TestClass objTest = new TestClass();
+            int changeCounter = 0;
+            PropertyChangedEventHandler handler1 = new PropertyChangedEventHandler((s, e) => { changeCounter++; });
+            PropertyChangedEventHandler handler2 = new PropertyChangedEventHandler((s, e) => { changeCounter++; });
+
+            objTest.PropertyChanged += handler1;
+            objTest.PropertyChanged += handler1;    //should not duplicate
+            objTest.PropertyChanged += handler1;    //should not duplicate
+            objTest.PropertyChanged += handler2;
+            objTest.PropertyChanged += handler2;   //should not duplicate
+
+            objTest.CleanProp = "test value";
+
+            Assert.IsTrue(changeCounter == 2, "Property change notifcation should only have 2 notifications.");
+
+        }
+
+        [TestMethod]
+        public void test_object_validation()
+        {
+            TestClass objTest = new TestClass();
+
+            Assert.IsFalse(objTest.IsValid, "Object should not be valid as one of its rules has not been set.");
+
+            objTest.StringProp = "some value";
+
+            Assert.IsTrue(objTest.IsValid, "Object should be valid as its property has been validated to be set.");
+
+        }
     }
 }
--- Core.Common.Tests/Test Classes/TestClass.cs
+++ Core.Common.Tests/Test Classes/TestClass.cs
@@ -1,12 +1,100 @@
-﻿using System;
+﻿using Core.Common.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentValidation;
 
 namespace Core.Common.Tests.Test_Classes
 {
-    public class TestClass
+    internal class TestClass : ObjectBase
     {
+        string _CleanProp = string.Empty;
+        string _DirtyProp = string.Empty;
+        string _StringProp = string.Empty;
+        TestChild _Child = new TestChild();
+        TestChild _NotNavigableChild = new TestChild();
+
+        public string CleanProp
+        {
+            get
+            {
+                return _CleanProp;
+            }
+
+            set
+            {
+                if(_CleanProp == value)
+                {
+                    return;
+                }
+                _CleanProp = value;
+                OnPropertyChanged(() => CleanProp, false);
+            }
+        }
+
+        public string DirtyProp
+        {
+            get
+            {
+                return _DirtyProp;
+            }
+
+            set
+            {
+                if(_DirtyProp == value)
+                {
+                    return;
+                }
+                _DirtyProp = value;
+                OnPropertyChanged(() => DirtyProp, false);
+            }
+        }
+
+        public string StringProp
+        {
+            get
+            {
+                return _StringProp;
+            }
+
+            set
+            {
+                if(_StringProp == value)
+                {
+                    return;
+                }
+                _StringProp = value;
+                OnPropertyChanged(()=>_StringProp, false);
+            }
+        }
+        public TestChild Child
+        {
+            get
+            {
+                return _Child;
+            }
+        }
+
+        public TestChild NotNavigableChild
+        {
+            get
+            {
+                return _NotNavigableChild;
+            }
+
+        }
+        class TestClassValidator : AbstractValidator<TestClass>
+        {
+            public TestClassValidator()
+            {
+                RuleFor(obj => obj.StringProp).NotEmpty();
+            }
+        }
+        protected override IValidator GetValidator()
+        {
+            return new TestClassValidator();
+        }
     }
 }
